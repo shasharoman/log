15e93d123dd9216d09611044296417311d9eba2b
shasharoman <shasharoman.deng@amberaigroup.com>
2020-09-28 16:57:56 +0800

    feat: simple rotation function

--- index.js
+++ index.js
@@ -75,23 +75,84 @@ class Logger {
 }
 
 class FileTransport {
-    constructor(level, path) {
+    constructor(level, path, opts = {}) {
         mkdir(p.dirname(path));
 
         this.level = LEVEL[level] || 0;
+        this.path = path;
 
         this.pause = false;
         this.buffer = [];
+
+        if (opts.rotate) {
+            this._initRotate(opts.rotate);
+        }
+        this._openStream(path);
+    }
+
+    _initRotate(opts) {
+        let {
+            suffixFn,
+            interval = 300000 // 5min
+        } = opts;
+
+        if (!suffixFn) {
+            suffixFn = () => {
+                let now = new Date();
+                let year = (now.getFullYear() + '').substr(2);
+                let month = ((now.getMonth() + 1) + '').padStart(2, '0');
+                let day = (now.getDate() + '').padStart(2, '0');
+
+                return `${year}${month}${day}`;
+            };
+        }
+
+        let timer = setInterval(() => {
+            let suffix = suffixFn();
+            if (this.suffix === suffix) {
+                return;
+            }
+
+            this.suffix = suffix;
+            this._reopenStream();
+        }, interval);
+        timer.unref();
+
+        this.suffix = suffixFn();
+    }
+
+    _openStream() {
+        let path = this.path + (this.suffix ? `.${this.suffix}` : '');
 
         this.stream = fs.createWriteStream(path, {
             flags: 'a',
             encoding: 'utf8'
         });
-
         this.stream.on('drain', () => {
             this.pause = false;
             this._write();
         });
+    }
+
+    _reopenStream() {
+        this.stream.close();
+        this.stream = null;
+        this.pause = false;
+
+        this._openStream();
+    }
+
+    _write() {
+        if (this.pause || !this.stream) {
+            return;
+        }
+
+        let items = this.buffer.splice(0, this.buffer.length);
+        if (items.length === 0) {
+            return;
+        }
+
+        this.pause = !this.stream.write(items.join('\n'), 'utf8');
     }
 
     log(level = 0, ...args) {
@@ -115,19 +176,6 @@ class FileTransport {
 
         this.buffer.push(format.join(' ') + '\n');
         this._write();
-    }
-
-    _write() {
-        if (this.pause) {
-            return;
-        }
-
-        let items = this.buffer.splice(0, this.buffer.length);
-        if (items.length === 0) {
-            return;
-        }
-
-        this.pause = !this.stream.write(items.join('\n'), 'utf8');
     }
 }
 
--- test/log.js
+++ test/log.js
@@ -7,10 +7,13 @@ const logPath = p.resolve(__dirname, 'log');
 
 before(() => {
     fs.existsSync(logPath) && fs.unlinkSync(logPath);
+    for (let i = 1; i < 10; i++) {
+        fs.existsSync(`${logPath}.${i}`) && fs.unlinkSync(`${logPath}.${i}`);
+    }
 });
 
 describe('logger', () => {
-    it('level, debug', async () => {
+    it('level', async () => {
         let logger = new log.Logger('label', [
             new log.FileTransport('INFO', logPath)
         ]);
@@ -27,8 +30,52 @@ describe('logger', () => {
         expect(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \[WARN\] \[label\] warn/.test(lines[1])).to.be.equal(true);
         expect(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \[ERROR\] \[label\] error/.test(lines[2])).to.be.equal(true);
     });
+
+    it('rotate', async () => {
+        let i = 1;
+        let opts = {
+            rotate: {
+                interval: 3,
+                suffixFn: () => i
+            }
+        };
+        let logger = new log.Logger('label', [
+            new log.FileTransport('DEBUG', logPath, opts)
+        ]);
+
+        logger.debug('debug');
+        i++;
+        await new Promise(resolve => setTimeout(resolve, 10));
+
+        logger.info('info');
+        i++;
+        await new Promise(resolve => setTimeout(resolve, 10));
+
+        logger.warn('warn');
+        i++;
+        await new Promise(resolve => setTimeout(resolve, 10));
+
+        logger.error('error');
+        i++;
+        await new Promise(resolve => setTimeout(resolve, 10));
+
+        await new Promise(resolve => setTimeout(resolve, 10));
+        let debug = fs.readFileSync(`${logPath}.1`).toString().split('\n');
+        let info = fs.readFileSync(`${logPath}.2`).toString().split('\n');
+        let warn = fs.readFileSync(`${logPath}.3`).toString().split('\n');
+        let error = fs.readFileSync(`${logPath}.4`).toString().split('\n');
+
+        expect(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \[DEBUG\] \[label\] debug/.test(debug[0])).to.be.equal(true);
+        expect(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \[INFO\] \[label\] info$/.test(info[0])).to.be.equal(true);
+        expect(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \[WARN\] \[label\] warn/.test(warn[0])).to.be.equal(true);
+        expect(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \[ERROR\] \[label\] error/.test(error[0])).to.be.equal(true);
+    });
 });
 
 after(() => {
     fs.existsSync(logPath) && fs.unlinkSync(logPath);
+
+    for (let i = 1; i < 10; i++) {
+        fs.existsSync(`${logPath}.${i}`) && fs.unlinkSync(`${logPath}.${i}`);
+    }
 });
